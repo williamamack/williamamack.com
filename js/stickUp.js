3ad7c5050ab1859a2952290ab5cb996e3e9dcd01
Will Mack <williamamack@gmail.com>
2018-07-05 11:50:14 -0400

    Scrolling fix
    This is accommodate .offset() behavior in different browsers

--- js/stickUp.js
+++ js/stickUp.js
@@ -70,7 +70,7 @@ function($) {
 			if(menuSize != null){
 				for(var i=0;i < menuSize;i++)
 				{
-					contentTop[i] = $('#'+content[i]+'').offset().top;
+					contentTop[i] = parseInt($('#'+content[i]+'').offset().top);
 					function bottomView(i) {
 						contentView = $('#'+content[i]+'').height()*.4;
 						testView = contentTop[i] - contentView;
